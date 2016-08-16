653a83174b69e73825d4c54729056423327c1d7a
Ubuntu <ubuntu@ip-172-31-23-141.us-west-2.compute.internal>
2016-08-16 23:50:15 +0000

    improve email viewing

--- about/emails.md
+++ about/emails.md
@@ -6,4 +6,5 @@ permalink: /about/emails/
 
 **Bug:** The timestamps are incorrect in the database.
 
-<div class="widget" data-type="table" data-url="https://db.seattlepublicrecords.org/query/?q=" data-query="SELECT FSLOCATION, DATETIME, EMAILFROM, EMAILTO, SUBJECT FROM emails ORDER BY DATETIME DESC LIMIT 10"></div>
+<div class="widget" data-type="table" data-hide-ifnohashurl="true" data-url="https://db.seattlepublicrecords.org/query/?q=" data-name="view_email" data-table="emails" data-layout="vertical" data-hide-fields="fslocation" data-allow-query-edit="false"></div>
+<div class="widget" data-type="table" data-url="https://db.seattlepublicrecords.org/query/?q=" data-name="emails_list" data-query="SELECT FSLOCATION, EMAILFROM, EMAILTO, SUBJECT, DATETIME FROM emails ORDER BY DATETIME DESC LIMIT 10" data-hide-fields="fslocation" data-whole-row-link="#!*for=view_email&fslocation={fslocation}" data-layout="horizontal"></div>
--- js/main.js
+++ js/main.js
@@ -1,42 +1,155 @@
-function makeWidget(element, data) {
-  console.log(element, JSON.stringify(data));
-  console.log('third', element.attr('data-type'))
-  var widget_html = '';
-  if (element.attr('data-type') == 'table') {
-    widget_html += '<table><tr>';
-    $.each(data['fields'], function(i, field) {
-      widget_html += '<td>'+field+'</td>';
-    })
-    widget_html += '</tr>';
-    $.each(data['rows'], function(i, row) {
-      widget_html += '<tr>';
-      $.each(data['fields'], function(j, field) {
-        var cell = row[field];
-        if (Array.isArray(cell)) {
-            cell = JSON.stringify(row[field]);
+window.firstTimeLoad = true;
+
+Array.prototype.remove = function() {
+    var what, a = arguments,
+        L = a.length,
+        ax;
+    while (L && this.length) {
+        what = a[--L];
+        while ((ax = this.indexOf(what)) !== -1) {
+            this.splice(ax, 1);
         }
-        widget_html += '<td>'+cell+'</td>';
-      })
-      widget_html += '</tr>';
-    })
-    widget_html += '</table>';
-  }
-  console.log(widget_html);
-  element.html(widget_html);
+    }
+    return this;
+};
+String.prototype.replaceAll = function(search, replacement) {
+    var target = this;
+    return target.replace(new RegExp(search, 'g'), replacement);
+};
+function range(start, stop, step) {
+    if (typeof stop == 'undefined') {
+        // one param defined
+        stop = start;
+        start = 0;
+    }
+
+    if (typeof step == 'undefined') {
+        step = 1;
+    }
+
+    if ((step > 0 && start >= stop) || (step < 0 && start <= stop)) {
+        return [];
+    }
+
+    var result = [];
+    for (var i = start; step > 0 ? i < stop : i > stop; i += step) {
+        result.push(i);
+    }
+
+    return result;
+};
+function makeWidget(element, data) {
+    console.log(element, JSON.stringify(data));
+    console.log('third', element.attr('data-type'))
+    var widget_html = '';
+    var limitOptions = '';
+    console.log('RANGE', range(1,11));
+    $.each(range(1,11), function(i, n) {
+        limitOptions += '<option>'+(n*10) +'</option>';
+    });
+    if (!(element.attr('data-allow-query-edit') == 'false')) {
+        widget_html += '<strong>Limit</strong> <select>'+limitOptions+'</select>'; 
+    }
+    if (element.attr('data-type') == 'table') {
+        widget_html += '<table class="data_table">';
+        var layout = 'horizontal';
+        if (element.attr('data-layout') == 'horizontal') {
+            widget_html += '<tr>';
+        }
+        
+        var fields = data['fields'];
+        if (element.attr('data-hide-fields')) {
+            var fields_to_hide = element.attr('data-hide-fields').split(',');
+            $.each(fields_to_hide, function(i, field) {
+                fields.remove(field);
+            });
+        }
+        if (element.attr('data-layout') == 'horizontal') {
+            $.each(fields, function(i, field) {
+                widget_html += '<th>' + field + '</th>';
+            })
+            widget_html += '</tr>';
+        }
+        $.each(data['rows'], function(i, row) {
+            
+            var whole_row_link = '';
+            if (element.attr('data-whole-row-link')) {
+                var link = element.attr('data-whole-row-link');
+                link = link.replace('{fslocation}', data['rows'][i]['fslocation'])
+                whole_row_link = ' class="linked_row" data-whole-row-link="' + link + '"';
+            }
+            widget_html += '<tr' + whole_row_link + '>';
+            $.each(fields, function(j, field) {
+                if (element.attr('data-layout') != 'horizontal') {
+                    widget_html += '<tr><th>'+field+'</th>';
+                }
+                var cell = row[field];
+
+                if(Array.isArray(cell)) {
+                    cell = JSON.stringify(row[field]).replaceAll('","', '", "');
+                } else if (cell.indexOf('http') > -1 && cell.indexOf(' ') == -1) {
+                    cell = '<a href="' + cell + '">' + cell + '</a>';
+                } else if (cell.indexOf('\n') > -1) {
+                    cell = cell.replaceAll('\n', '<br/>');
+                }
+                widget_html += '<td>' + cell + '</td>';
+                if (element.attr('data-layout') != 'horizontal') {
+                    widget_html += '</tr>';
+                }
+            })
+            if (element.attr('data-layout') == 'horizontal') {
+                widget_html += '</tr>';
+            }
+        })
+        widget_html += '</table>';
+    }
+    console.log(widget_html);
+    element.html(widget_html);
 }
+
 function runWidgets() {
-  console.log('running widgets');
-  $('.widget').each(function() {
-    console.log('first');
-    var url = $(this).attr('data-url') + encodeURIComponent($(this).attr('data-query'));
-    var element = $(this);
-    element.html('Data is loading')
-    $.get(url, function(data) {
-      console.log('second');
-      makeWidget(element, data);
+    console.log('running widgets');
+    var url_parms = [];
+    var hashFor = [];
+    $.each(window.location.hash.slice(2).split('&'), function(i, item) {
+        console.log(item.split('=')[0]);
+        if (item.split('=')[0] == '*for') {
+            hashFor.push(item[1]);
+        } else {
+            url_parms.push(item.split('='));
+        }
+    });
+    $('.widget').each(function() {
+        var element = $(this);
+        if (!$(this).attr('data-query')) {
+            
+            var wheres = '';
+            $.each(url_parms, function(i, item) {
+                wheres += item[0] + " = '" + item[1] + "'";
+                if (i < (url_parms.length - 1)) {
+                    wheres += ' AND ';
+                }
+            });
+            var query = 'SELECT * FROM ' + element.attr('data-table') + ' WHERE ' + wheres;
+            var url = $(this).attr('data-url') + encodeURIComponent(query);
+        } else {
+            var url = $(this).attr('data-url') + encodeURIComponent($(this).attr('data-query'));
+        }
+        if (!(element.attr('data-hide-ifnohashurl') == 'true' && (hashFor.indexOf(element.attr('data-name')) > -1))) {
+            element.html('Data is loading')
+            $.get(url, function(data) {
+                console.log('second');
+                makeWidget(element, data);
+            })
+        }
     })
-  })
 }
-  $(function() {
+$(function() {
     runWidgets();
-  })
+    $('body').on('click', '.linked_row', function() {
+        window.location = $(this).attr('data-whole-row-link');
+    })
+    $(window).on('hashchange', function() {
+        runWidgets();
+    });
+})
